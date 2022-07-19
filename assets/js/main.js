8bcdb01ac034b01504e7b69cc974edd09ea2c347
Zebbus <47741562+Zebbus@users.noreply.github.com>
2022-07-19 21:49:29 +0200

    Updated text | added menu | added font

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,29 @@
+// Variables
 var font;
 var site = document.getElementsByTagName('body')[0];
+var lights = document.getElementById('lights');
 var dark = document.getElementById('darkmode');
+var light = document.getElementById('lightmode');
+var logo = document.getElementById('logo')
+var homeNav = document.getElementById('home-nav');
+var workNav = document.getElementById('work-nav');
+var friendsNav = document.getElementById('friends-nav');
+var foodNav = document.getElementById('food-nav');
+var home = document.getElementById('home');
+var work = document.getElementById('work');
+var friends = document.getElementById('friends');
+var food = document.getElementById('food');
 var hour = new Date().getHours();
 
-if(localStorage.darkmode === 'true'|| hour >= 22 || hour <= 7){
-    site.classList.add("darkmode");
-    dark.innerHTML = 'light_mode';
-};
+// Functions
 
-window.addEventListener('load', function() {
+function hide(bye){
+    bye.style.display = 'none';
+}
+
+function show(hi){
+    hi.style.display = '';
+}
 
 function pickFont(){
     fonts = ['Aladin','Georgia','Amatic SC','Bebas Neue', 'Creepster','Limelight','Press Start 2P', 'Rubik Glitch', 'Special Elite']
@@ -32,24 +47,32 @@ function setAll(){
     for (let e of document.getElementsByClassName("logo")) { e.style.fontFamily = font; }
 };
 
+function menu(option){
+    for(var e of document.getElementsByClassName('content')) {e.style.display = 'none';}
+        show(option);
+    if(option.id === 'home'){
+        hide(homeNav);
+    }
+    else {
+        show(homeNav);
+    }
+}
+
 function darkMode(){
-    console.log('why')
     if(site.classList.contains('darkmode')){
         site.style.transition = 'all 1s';
         site.classList.remove("darkmode");
-        dark.innerHTML = 'dark_mode';
+        hide(light);
+        show(dark);
         localStorage.setItem("darkmode", false);     
     }
     else {
         site.style.transition = 'all 1s';
         site.classList.add("darkmode");
-        dark.innerHTML = 'light_mode';
+        hide(dark);
+        show(light);
         localStorage.setItem("darkmode", true); 
     }
-};
-
-dark.onclick = function(){
-    darkMode();
 };
 
 (function switchUpAll(){
@@ -60,5 +83,27 @@ dark.onclick = function(){
     }, rand);
 }());
 
+// On clicks
 
-  });
+workNav.onclick = function() {menu(work)}
+
+friendsNav.onclick = function() {menu(friends)}
+
+foodNav.onclick = function() {menu(food)};
+
+homeNav.onclick = function() {menu(home)};
+
+logo.onclick = function() {menu(home)};
+
+lights.onclick = function(){darkMode()};
+
+// Darkmode if stored or night time
+if(localStorage.darkmode === 'true'|| hour >= 22 || hour <= 7){
+    site.classList.add("darkmode");
+    hide(dark);
+    show(light);
+}
+else{
+    hide(light);
+    show(dark);
+};
